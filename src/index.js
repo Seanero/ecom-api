9615b19281fb93b7f9dc4ac0ddc019759d130ac4
Aurélien GABORIT <aureliengaborit@Ordinateur-portable-de-Aurelien.local>
2025-03-27 17:22:55 +0100

    Add main routes + correct import body-parser

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = ("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
+
+const mainRoutes = require("./routes/index");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+
+
+app.use("/", mainRoutes);
 
 const startServer = async () => {
     app.listen(process.env.WEB_PORT, () => {
